916ca0d1ab5ff353e67f2f710b37e0ebff0e9039
Prathmesh <patilprathmesh050203@gmail.com>
2024-01-02 15:26:57 +0530

    project done

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
+
+const app=express()
+app.use(cors())
+const server=http.createServer(app)
+
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log(`User connected: ${socket.id}`)
+
+    socket.on("join_room",(data)=>{
+        socket.join(data)
+    })
+
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("receive_message",data)
+    })
+})
+
+server.listen(5000,()=>{
+    console.log("Server is Running")
+})
